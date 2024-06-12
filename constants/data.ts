69eda4869a5c9326f02897664c1de49315ea7b77
Compulink-Dev <devcompulink@gmail.com>
2024-06-12 15:54:14 +0200

    main

---
+++ constants/data.ts
@@ -0,0 +1,154 @@
+export const links = [
+  {
+    id: 1,
+    name: "Home",
+    link: "/",
+  },
+  {
+    id: 2,
+    name: "What We Do?",
+    link: "/about",
+  },
+  {
+    id: 3,
+    name: "Downloads",
+    link: "downloads",
+  },
+  {
+    id: 4,
+    name: "Programs",
+    link: "programs",
+  },
+  {
+    id: 5,
+    name: "Our Team",
+    link: "team",
+  },
+  {
+    id: 6,
+    name: "Contact",
+    link: "contact",
+  },
+  {
+    id: 7,
+    name: 'Vacancies',
+    link: 'vacancy'
+  }
+];
+
+export const miniGallery = [
+
+  {
+    id: 1,
+    imageUrl: '/ibc.JPG'
+  },
+  {
+    id: 2,
+    imageUrl: '/ibc2.JPG'
+  },
+  {
+    id: 3,
+    imageUrl: '/ibc3.JPG'
+  }, {
+    id: 4,
+    imageUrl: '/ibc4.JPG'
+  }, {
+    id: 5,
+    imageUrl: '/ibc5.JPG'
+  }, {
+    id: 6,
+    imageUrl: '/ibc6.JPG'
+  },
+
+]
+
+export const galleries = [
+  {
+    id: 1,
+    imageUrl: '/ibc.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 2,
+    imageUrl: '/ibc2.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 3,
+    imageUrl: '/ibc3.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 4,
+    imageUrl: '/ibc4.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 5,
+    imageUrl: '/ibc5.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 6,
+    imageUrl: '/ibc6.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 7,
+    imageUrl: '/ibc7.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 8,
+    imageUrl: '/ibc8.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 9,
+    imageUrl: '/ibc9.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 10,
+    imageUrl: '/ibc10.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 11,
+    imageUrl: '/ibc11.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 12,
+    imageUrl: '/ibc12.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 13,
+    imageUrl: '/ibc13.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+  {
+    id: 14,
+    imageUrl: '/ibc14.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 15,
+    imageUrl: '/ibc15.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  }, {
+    id: 16,
+    imageUrl: '/ibc16.JPG',
+    title: 'Necf',
+    desc: 'Necf'
+  },
+]
