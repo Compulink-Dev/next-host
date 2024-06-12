bc2a6787a249e82f219c43d13fd59be3d03d32db
Compulink-Dev <devcompulink@gmail.com>
2024-06-12 16:30:21 +0200

    main

--- lib/vacancy/getVacancies.ts
+++ lib/vacancy/getVacancies.ts
@@ -1,6 +1,7 @@
 import Vacancy from "@/models/vacancy";
 import { connectToDB } from "../connectToDB";
 
+
 export default async function getVacancies() {
   await connectToDB()
   const vacancies = await Vacancy.find()
