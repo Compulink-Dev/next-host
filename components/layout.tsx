d9ed494eb35c07880416fa34fa8ecfc96968d42d
Compulink-Dev <devcompulink@gmail.com>
2024-06-12 15:57:05 +0200

    main

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,17 +1,14 @@
 import React from 'react'
 import Navbar from './navbar'
-import Footer from '@/app/(main)/_components/footer'
-import MainHero from '@/app/(main)/_components/mainhero'
+
 import ScrollToTop from './scrollToTop'
 
 function MainLayout({ children }: { children: React.ReactNode }) {
     return (
         <div>
             <Navbar />
-            <MainHero />
             {children}
             <ScrollToTop />
-            <Footer />
         </div>
     )
 }
